8633a8913ad8d2cc36dbc46b89c5d4e1941d92d5
Misha <abachyanm@gmail.com>
2020-11-22 21:07:24 +0400

    First test

--- js/form.js
+++ js/form.js
@@ -1,17 +1,82 @@
-let selectData = [];
+function callForm({
+                      url: url,
+                      action: action
+                  }) {
+    /*Get data from API*/
 
-fetch('https://reqres.in/api/users?page=1')
-    .then((req)=>{
-        return req.json()
-    })
-.then((res)=>{
-    res.data.map((e)=>{
-        getUserData(e.first_name, e.last_name)
-    })
-});
+    fetch(url)
+        .then((req) => {
+            return req.json()
+        })
+        .then((res) => {
+            res.data.map((e) => {
+                getUserData(e.first_name, e.last_name)
+            })
+        });
 
 
-function getUserData(first_name, last_name) {
-    $('#cont_name').append('<option>' + `${first_name}  ${last_name}` + '</option>' );
-}
+    function getUserData(first_name, last_name) {
+        $('#cont_name').append('<option value=' + `\"${first_name} ${last_name}"/` + '>' + `${first_name}  ${last_name}` + '</option>');
+    }
 
+    /*End of get data*/
+
+
+    /*Form submission*/
+
+    function createForm() {
+        $('.form-block')
+            .append(
+                '<form id="form-main" action="">' +
+                '<div class="name-date-row">' +
+                '<select name="contact_name" id="cont_name" required>\n' + '<option value="" disabled selected>Select Name</option>\n' + '</select>' +
+                '<input data-toggle="datepicker" placeholder="Select Date" class="date-select" required>' +
+                '</div>' +
+                '<textarea name="" cols="30" rows="10" class="text-msg"></textarea>' +
+                '<button type="submit" class="submit-btn">Submit</button>' +
+                '</form>')
+    }
+
+    createForm();
+
+    $('.submit-btn').on('click', function (event) {
+        event.preventDefault();
+        let fullName = $('#cont_name').val();
+        let selectedDate = $('.date-select').val();
+        let textMessage = $('.text-msg').val();
+        if(fullName == null || selectedDate == ''){
+
+        }else{
+            createModal();
+            $('.selected-name').val(fullName);
+            $('.selected-date').val(selectedDate);
+            $('.selected-message').val(textMessage);
+        }
+    });
+
+    function createModal() {
+        $('body').append(
+            '<div class="modal-cover"></div>'+
+            '<div class="form-modal">' +
+            '<form action=' + `${action}` + '>' +
+            '<label for="modal-selected-name">Selected Name</label>'+
+            '<input id="modal-selected-name" class="selected-name" disabled>' +
+            '<label for="modal-selected-date">Selected Date</label>'+
+            '<input id="modal-selected-date" class="selected-date" disabled>' +
+            '<label for="modal-selected-msg">Message</label>'+
+            '<textarea id="modal-selected-msg" class="selected-message" disabled></textarea>' +
+            '<button type="submit" class="modal-submit-btn">Submit</button>' +
+            '</form>' +
+            '</div>'
+        );
+        $('.modal-cover').on('click', function (event) {
+            event.stopPropagation();
+            if ($(event.target).has('modal-cover')){
+                $('.form-modal').remove();
+                $('.modal-cover').remove();
+            }else{
+
+            }
+        })
+    }
+    }
---
+++ js/script.js
@@ -0,0 +1,10 @@
+callForm({
+    url: 'https://reqres.in/api/users?page=1',
+    action: './form-submitted.html'
+});
+
+/*DataPicker Init*/
+
+$('[data-toggle="datepicker"]').datepicker({});
+
+/*Endf of DatePicker Init*/
